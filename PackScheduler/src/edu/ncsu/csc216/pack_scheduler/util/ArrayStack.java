f964ef4e0629eaa3dbd5527a2e43f6ba5ee3524a
epsvanes <epsvanes@ncsu.edu>
2022-11-08 18:42:47 -0500

    peer programming- driver: erik, guiders: devin & drew

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayStack.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayStack.java
@@ -6,9 +6,10 @@ package edu.ncsu.csc216.pack_scheduler.util;
 import java.util.EmptyStackException;
 
 /**
- * Class to implement Stack with underlying array list. 
+ * Class to implement Stack with underlying array list.
+ * 
  * @author devinmowry
- * @param <E> generic element type 
+ * @param <E> generic element type
  *
  */
 public class ArrayStack<E> implements Stack<E> {
@@ -18,18 +19,25 @@ public class ArrayStack<E> implements Stack<E> {
 	private int size;
 	/** The capacity of the stack */
 	private int capacity;
-	
+
 	/**
 	 * The constructor of the array stack, which takes in the capacity
 	 * 
 	 * @param capacity the capacity for the stack
 	 */
-	public ArrayStack(int capacity){
+	public ArrayStack(int capacity) {
 		arrList = new ArrayList<E>();
 		setCapacity(capacity);
 		size = 0;
 	}
-	
+
+	/**
+	 * Method that adds an element onto the top of the stack
+	 * 
+	 * @param element the element to add to the stack
+	 * @throws IllegalArgumentException if there is a problem with the parameter or
+	 *                                  the stack is full
+	 */
 	@Override
 	public void push(E element) throws IllegalArgumentException {
 		if (element == null) {
@@ -39,23 +47,50 @@ public class ArrayStack<E> implements Stack<E> {
 			throw new IllegalArgumentException("");
 		}
 		arrList.add(element);
+		size++;
 	}
 
+	/**
+	 * Method that removes the top element in the stack and returns it
+	 * 
+	 * @return the element that is removed
+	 * @throws EmptyStackException if the stack is empty
+	 */
 	@Override
 	public E pop() throws EmptyStackException {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+		size--;
 		return arrList.remove(size - 1);
 	}
 
+	/**
+	 * Method that checks whether the stack is empty by checking the size
+	 * 
+	 * @return if the stack is empty, (if the size is 0)
+	 */
 	@Override
 	public boolean isEmpty() {
 		return size == 0;
 	}
 
+	/**
+	 * Method that returns the size of the stack
+	 * 
+	 * @return the size of the stack
+	 */
 	@Override
 	public int size() {
 		return size;
 	}
 
+	/**
+	 * Method that sets the capacity to the parameter if it is valid
+	 * 
+	 * @param capacity the new capacity for the stack
+	 * @throws IllegalArgumentException if the parameter is invalid
+	 */
 	@Override
 	public void setCapacity(int capacity) throws IllegalArgumentException {
 		if (capacity < size) {
