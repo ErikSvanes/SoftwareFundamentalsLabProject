f87604e063352bfcda97dc2f3d200fc0758716de
epsvanes <epsvanes@ncsu.edu>
2022-11-29 18:17:38 -0500

    pair programming: driver- erik, guiders- devin & drew

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursive.java
@@ -0,0 +1,202 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.pack_scheduler.util;
+
+/**
+ * This is the recursive linked list class.
+ * 
+ * @author Erik
+ * @param <E> so that it can use any variable
+ *
+ */
+public class LinkedListRecursive<E> {
+
+	/** The front list node of the list */
+	private ListNode front;
+	/** The size of the list */
+	private int size;
+
+	/**
+	 * The constructor
+	 */
+	public LinkedListRecursive() {
+		front = null;
+		size = 0;
+	}
+
+	/**
+	 * Returns if the list is empty
+	 * 
+	 * @return if the list is empty
+	 */
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	/**
+	 * Returns the size of the list
+	 * 
+	 * @return the size of the list
+	 */
+	public int size() {
+		return size;
+	}
+
+	/**
+	 * This method adds an element to the end of the list.
+	 * 
+	 * @param element the element to add
+	 * @return true if the element is added
+	 */
+	public boolean add(E element) {
+		if (isEmpty() || contains(element)) {
+			throw new IllegalArgumentException();
+		}
+		size++;
+		return front.add(element);
+	}
+
+	/**
+	 * This method adds an element to the specified index in the list.
+	 * 
+	 * @param index   the index to add the element at
+	 * @param element the element to add to the list
+	 */
+	public void add(int index, E element) {
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException();
+		}
+		else if (element == null) {
+			throw new NullPointerException();
+		}
+		else if (contains(element)) {
+			throw new IllegalArgumentException();
+		}
+		else if (index == 0) {
+			front = new ListNode(element, front);
+		}
+		else {
+			front.add(index, element);
+		}
+	}
+
+	/**
+	 * asdf
+	 * 
+	 * @param index asdf
+	 * @return asdf
+	 */
+	public E get(int index) {
+		return null;
+	}
+
+	/**
+	 * asdf
+	 * 
+	 * @param element asdf
+	 * @return asdf
+	 */
+	public boolean remove(E element) {
+		return true;
+	}
+
+	/**
+	 * asdf
+	 * 
+	 * @param index asdf
+	 * @return asdf
+	 */
+	public E remove(int index) {
+		return null;
+	}
+
+	/**
+	 * asdf
+	 * 
+	 * @param index   asdf
+	 * @param element asdf
+	 */
+	public void set(int index, E element) {
+
+	}
+
+	/**
+	 * This method checks the list to see if it contains the given method from the
+	 * parameter.
+	 * 
+	 * @param element the element to check for in the list
+	 * @return true or false whether the element is found in the list
+	 */
+	public boolean contains(E element) {
+		if (front == null) {
+			throw new IllegalArgumentException();
+		}
+		return front.contains(element);
+	}
+
+	private class ListNode {
+		/** The data for the list node */
+		public E data;
+		/** The next list node pointer */
+		public ListNode next;
+
+		/**
+		 * The list node constructor which creates a list node and sets the local
+		 * variables
+		 * 
+		 * @param data the data for the list node
+		 * @param next the next list node pointer
+		 */
+		public ListNode(E data, ListNode next) {
+			this.data = data;
+			this.next = next;
+		}
+
+		/**
+		 * This is the private helper method that is called recursively for the contains
+		 * method.
+		 * 
+		 * @param element the element to check the data on
+		 * @return true if the element matches the data, and false if the node is null
+		 */
+		public boolean contains(E element) {
+			if (data.equals(element)) {
+				return true;
+			} else if (next == null) {
+				return false;
+			}
+			return next.contains(element);
+		}
+
+		/**
+		 * This is the private helper method that is called recursively for the add
+		 * method.
+		 * 
+		 * @param element the element to add at the end of the list
+		 * @return true if the element is added
+		 */
+		public boolean add(E element) {
+			if (next == null) {
+				next = new ListNode(element, null);
+				return true;
+			}
+			return next.add(element);
+		}
+
+		/**
+		 * This is the private helper method that is called recursively for the add
+		 * method at an index.
+		 * 
+		 * @param index the index to add the element to
+		 * @param element the element to add to the list
+		 */
+		public void add(int index, E element) {
+			if (index == 0) {
+				next = new ListNode(element, next);
+				return;
+			}
+			next.add(index - 1, element);
+		}
+	}
+}
---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursiveTest.java
@@ -0,0 +1,112 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+
+class LinkedListRecursiveTest {
+
+	/**
+	 * Tests the ArrayList constructor
+	 */
+	@Test
+	public void testConstructor() {
+		LinkedList<String> list = new LinkedList<String>();
+		assertEquals(0, list.size());
+	}
+	/**
+	 * Tests the ArrayList add method
+	 */
+	@Test
+	public void testAdd() {
+		LinkedList<String> list = new LinkedList<String>();
+		list.add(0, "a");
+		list.add(1, "b");
+		list.add(2, "c");
+		list.add(3, "d");
+		list.add(4, "e");
+		assertEquals("d", list.get(3));
+		list.add(3, "z");
+		assertEquals("z", list.get(3));
+		assertEquals("d", list.get(4));
+		list.add(0, "l");
+		assertEquals("l", list.get(0));
+		assertEquals("a", list.get(1));
+		ListIterator interator = list.listIterator(2);
+		assertTrue(interator.hasNext());
+	}
+	/**
+	 * Tests the ArrayList grow method
+	 */
+	@Test 
+	public void testGrowArray() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		list.add(7);
+		list.add(8);
+		list.add(9);
+		list.add(10);
+		list.add(11);
+		list.add(12);
+		
+		assertEquals(list.size(), 12);
+	}
+	
+	/**
+	 * Tests the ArrayList remove method
+	 */
+	@Test 
+	public void testRemove() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		assertEquals(list.size(), 6);
+		ListIterator<Integer> iterator = list.listIterator(0);
+		assertThrows(NoSuchElementException.class, () -> iterator.previous());
+		iterator.next();
+		iterator.remove();
+		assertEquals(list.size(), 5);
+		iterator.next();
+		iterator.next();
+		iterator.previous();
+		iterator.remove();
+		for(int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		assertEquals(list.size(), 4);
+//		for(int i = 0; i< list.size(); i++) {
+//			System.out.println(list.get(i));
+//		}
+//		System.out.println(list.size());
+	}
+	
+	/**
+	 * Tests the ArrayList set method
+	 */
+	@Test
+	public void testSet() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		assertThrows(IllegalArgumentException.class,
+				() -> list.set(0, 2));
+		assertTrue(list.set(0, 7).equals(1));
+		assertTrue(list.get(0).equals(7));
+	}
+}
